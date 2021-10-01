b3e6ac178e52e188a2dfa5c16ff10236fba61dcb
Nathan Henrie <nate@n8henrie.com>
2021-10-01 10:09:41 -0600

    Lint with black

--- speed.py
+++ speed.py
@@ -24,14 +24,17 @@ def avg(l):
 
 
 def main(server=1773, runs=3, drop_outliers=False, outfile=None):
-    speeds = {'Download': [], 'Upload': []}
+    speeds = {"Download": [], "Upload": []}
 
-    speedtest = pathlib.Path(sys.prefix) / 'bin' / 'speedtest-cli'
+    speedtest = pathlib.Path(sys.prefix) / "bin" / "speedtest-cli"
     if not speedtest.is_file():
-        speedtest = 'speedtest-cli'
+        speedtest = "speedtest-cli"
     for _ in range(runs):
-        cmd = subprocess.run([speedtest, '--server', str(server)],
-                             stdout=subprocess.PIPE, universal_newlines=True)
+        cmd = subprocess.run(
+            [speedtest, "--server", str(server)],
+            stdout=subprocess.PIPE,
+            universal_newlines=True,
+        )
         raw_output = cmd.stdout
         lines = raw_output.splitlines()
 
@@ -40,8 +43,9 @@ def main(server=1773, runs=3, drop_outliers=False, outfile=None):
             speeds[updown].append(speed)
 
     dt = datetime.now()
-    output = ("Test started at {}\n"
-              "Averaging results over {} runs\n".format(dt, runs))
+    output = "Test started at {}\n" "Averaging results over {} runs\n".format(
+        dt, runs
+    )
     for updown in speeds.keys():
         if drop_outliers and len(speeds[updown]) > 4:
             trimmed_speeds = sorted(speeds[updown])[1:-1]
@@ -49,33 +53,42 @@ def main(server=1773, runs=3, drop_outliers=False, outfile=None):
         else:
             trimmed_speeds = speeds[updown]
             trimmed_msg = ""
-        output += ("{} average{}: {:.02f}\n".format(updown, trimmed_msg,
-                                                    avg(trimmed_speeds)))
+        output += "{} average{}: {:.02f}\n".format(
+            updown, trimmed_msg, avg(trimmed_speeds)
+        )
     if outfile is None:
         print(output)
     else:
-        with open(outfile, 'a') as f:
-            f.write("{0:%Y-%m-%d %H:%M},{1:.02f},{2:.02f},{3:.02f},{4:.02f},"
-                    "{5},{6}\n".format(
-                        dt,
-                        avg(speeds["Download"]),
-                        avg(sorted(speeds["Download"])[1:-1]),
-                        avg(speeds["Upload"]),
-                        avg(sorted(speeds["Upload"])[1:-1]),
-                        runs,
-                        bool(drop_outliers)))
+        with open(outfile, "a") as f:
+            f.write(
+                "{0:%Y-%m-%d %H:%M},{1:.02f},{2:.02f},{3:.02f},{4:.02f},"
+                "{5},{6}\n".format(
+                    dt,
+                    avg(speeds["Download"]),
+                    avg(sorted(speeds["Download"])[1:-1]),
+                    avg(speeds["Upload"]),
+                    avg(sorted(speeds["Upload"])[1:-1]),
+                    runs,
+                    bool(drop_outliers),
+                )
+            )
 
 
 def _cli():
     parser = argparse.ArgumentParser(argument_default=argparse.SUPPRESS)
-    parser.add_argument('-s', '--server', help="Specify nondefault server")
-    parser.add_argument('-r', '--runs', type=int, help="Number of runs")
-    parser.add_argument('--drop-outliers', action='store_true',
-                        help="If runs >= 5, drop the highest and lowest")
-    parser.add_argument('-o', '--outfile',
-                        help="Append output to file instead of stdout")
+    parser.add_argument("-s", "--server", help="Specify nondefault server")
+    parser.add_argument("-r", "--runs", type=int, help="Number of runs")
+    parser.add_argument(
+        "--drop-outliers",
+        action="store_true",
+        help="If runs >= 5, drop the highest and lowest",
+    )
+    parser.add_argument(
+        "-o", "--outfile", help="Append output to file instead of stdout"
+    )
     args = parser.parse_args()
     return vars(args)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(**_cli())
